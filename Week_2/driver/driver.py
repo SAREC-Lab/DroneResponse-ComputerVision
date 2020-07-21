737049432a145a7322be32c5f07ede2f239bed14
patricksoga <psoga@nd.edu>
2020-07-21 11:25:37 -0700

    updated driver

--- Week_2/driver/driver.py
+++ Week_2/driver/driver.py
@@ -1,5 +1,3 @@
-#!/home/psoga/anaconda3/envs/ensemble/bin/python
-
 import cv2
 import os
 import json
@@ -29,37 +27,37 @@ def make_prediction(model, scaler, features):
   return model.predict(rescaled_feature)[0]
 
 def main():
-  id = 0
-  for arg in sys.argv[1:]:
-    id = arg
+  with open('./droneconfig.json') as config_json:
+    data = json.load(config_json)
+    id = data['drone1']['id']
 
   previous_condition = json.dumps("")
 
   sio = socketio.Client()
-  sio.connect('http://localhost:4201')
+  sio.connect('http://localhost:4202')
 
   size = tuple((500, 500))
 
-  cloudy_model  = load('./Cloudy.joblib')
-  cloudy_scaler = load('./Cloudy_scaler.joblib')
+  cloudy_model    = load('./Cloudy.joblib')
+  cloudy_scaler   = load('./Cloudy_scaler.joblib')
 
-  sunny_model  = load('./Sunny.joblib')
-  sunny_scaler = load('./Sunny_scaler.joblib')
+  sunny_model     = load('./Sunny.joblib')
+  sunny_scaler    = load('./Sunny_scaler.joblib')
   
-  rainy_model  = load('./rainy-svm-model.joblib')
-  rainy_scaler = load('./rainy-scaler.joblib')
+  rainy_model     = load('./rainy-svm-model.joblib')
+  rainy_scaler    = load('./rainy-scaler.joblib')
 
-  snowy_model  = load('./snowy-lr-model.joblib')
-  snowy_scaler = load('./snowy-scaler.joblib')
+  snowy_model     = load('./snowy-lr-model.joblib')
+  snowy_scaler    = load('./snowy-scaler.joblib')
 
-  foggy_model  = load('./foggy-svm-model.joblib')
-  foggy_scaler = load('./foggy-scaler.joblib')
+  foggy_model     = load('./foggy-svm-model.joblib')
+  foggy_scaler    = load('./foggy-scaler.joblib')
 
   daylight_model  = load('./daylight-classifier.joblib')
   daylight_scaler = load('./daylight-scaler.joblib')
 
-  cap = cv2.VideoCapture('../demo-videos/weather.mp4')
-  # os.popen("vlc ../daylight-stream-demo/daylight.mp4")
+  cap = cv2.VideoCapture('../demo-videos/weathertest.mp4')
+  os.popen("vlc ../demo-videos/weathertest.mp4")
 
   while(True):
     ret, frame = cap.read()
@@ -76,11 +74,11 @@ def main():
       "id":         id,
       "data": {
         "cloudy":   np.int(make_prediction(cloudy_model, cloudy_scaler, cloudy_features)),
-        "sunny":    np.int(make_prediction(sunny_model, sunny_scaler, sunny_features)),
-        "rainy":    np.int(make_prediction(rainy_model, rainy_scaler, rainy_features)),
-        "snowy":    np.int(make_prediction(snowy_model, snowy_scaler, snowy_features)),
-        "foggy":    np.int(make_prediction(foggy_model, foggy_scaler, foggy_features)),
-        "dark" :    np.int(make_prediction(daylight_model, daylight_scaler, daylight_features))
+        "sunny" :   np.int(make_prediction(sunny_model, sunny_scaler, sunny_features)),
+        "rainy" :   np.int(make_prediction(rainy_model, rainy_scaler, rainy_features)),
+        "snowy" :   np.int(make_prediction(snowy_model, snowy_scaler, snowy_features)),
+        "foggy" :   np.int(make_prediction(foggy_model, foggy_scaler, foggy_features)),
+        "dark"  :   np.int(make_prediction(daylight_model, daylight_scaler, daylight_features))
       }
     }
 
@@ -88,7 +86,7 @@ def main():
     if not json_weather_conditions == previous_condition:
       print(f'sent data from {id}: {json_weather_conditions}')
       previous_condition = json_weather_conditions
-      sio.emit('weather', json_weather_conditions)
+      sio.emit('jetsonConnection', json_weather_conditions)
     else:
       print(f'redundant data: {json_weather_conditions}')
 
